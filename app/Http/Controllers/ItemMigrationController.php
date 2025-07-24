c9de30d68bed43b58673f95ba1bced80e292a9bf
Fortune Bekee <fortubeks@gmail.com>
2025-07-24 22:14:58 +0100

    .

--- app/Http/Controllers/ItemMigrationController.php
+++ app/Http/Controllers/ItemMigrationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\ItemMigration;
 use App\Models\ItemMigrationDetail;
 use App\Models\Outlet;
+use App\Models\OutletStoreItem;
 use App\Models\Store;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
--- app/Models/Store.php
+++ app/Models/Store.php
@@ -19,7 +19,7 @@ class Store extends Model
     public function storeItems()
     {
         return $this->belongsToMany(StoreItem::class, 'store_store_items')
-            ->withPivot('id', 'qty', 'unit_cost', 'batch_number', 'expiry_date')
+            ->withPivot('id', 'qty', 'unit_cost', 'batch_reference', 'expiry_date')
             ->withTimestamps();
     }
 }
