50b86878b99b95c31b89d6815191b337d61d5b44
Fortune Bekee <fortubeks@gmail.com>
2025-12-22 21:07:03 +0100

    .

--- resources/views/rocker-theme/orders/partials/order-modal.blade.php
+++ resources/views/rocker-theme/orders/partials/order-modal.blade.php
@@ -194,11 +194,15 @@
                 'phone': phone,
                 _token: '{{ csrf_token() }}'
             }, function(data) {
+                console.log(data);
                 $('#selected-customer-id').val(data.id);
                 html = `<div id="customer-option" class="customer-option" data-id="${data.id}">${data.first_name}, (${data.phone})</div>`;
                 $('#customer-results').html(html);
                 $('#step-customer').addClass('d-none');
                 $('#step-delivery').removeClass('d-none');
+
+                //update order info
+                updateOrderInformation('selected_customer_id');
             });
         });
 
