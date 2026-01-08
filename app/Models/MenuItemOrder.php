4a2faf6edf0c5a5394290084a3180eb2c4e40daa
Fortune Bekee <fortubeks@gmail.com>
2026-01-08 10:34:40 +0100

    .

--- app/Models/MenuItemOrder.php
+++ app/Models/MenuItemOrder.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class MenuItemOrder extends Model
 {
+    use SoftDeletes;
     protected $fillable = [
         'menu_item_id',
         'order_id',
